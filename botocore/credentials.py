b1dd70a732fd4cf4bd98e66e09344cdb0c5919f7
Aaron Staple <aaron.staple@gmail.com>
2019-09-24 12:09:16 -0700

    Remove subprocess

--- botocore/credentials.py
+++ botocore/credentials.py
@@ -18,7 +18,6 @@ import os
 import getpass
 import threading
 import json
-import subprocess
 from collections import namedtuple
 from copy import deepcopy
 from hashlib import sha1
@@ -147,15 +146,8 @@ class ProfileProviderBuilder(object):
                 profile_name, disable_env_vars,
             ),
             self._create_shared_credential_provider(profile_name),
-            self._create_process_provider(profile_name),
             self._create_config_provider(profile_name),
         ]
-
-    def _create_process_provider(self, profile_name):
-        return ProcessProvider(
-            profile_name=profile_name,
-            load_config=lambda: self._session.full_config,
-        )
 
     def _create_shared_credential_provider(self, profile_name):
         credential_file = self._session.get_config_variable('credentials_file')
@@ -900,76 +892,6 @@ class CredentialProvider(object):
                 raise PartialCredentialsError(provider=self.METHOD,
                                               cred_var=key_name)
         return found
-
-
-class ProcessProvider(CredentialProvider):
-
-    METHOD = 'custom-process'
-
-    def __init__(self, profile_name, load_config, popen=subprocess.Popen):
-        self._profile_name = profile_name
-        self._load_config = load_config
-        self._loaded_config = None
-        self._popen = popen
-
-    def load(self):
-        credential_process = self._credential_process
-        if credential_process is None:
-            return
-
-        creds_dict = self._retrieve_credentials_using(credential_process)
-        if creds_dict.get('expiry_time') is not None:
-            return RefreshableCredentials.create_from_metadata(
-                creds_dict,
-                lambda: self._retrieve_credentials_using(credential_process),
-                self.METHOD
-            )
-
-        return Credentials(
-            access_key=creds_dict['access_key'],
-            secret_key=creds_dict['secret_key'],
-            token=creds_dict.get('token'),
-            method=self.METHOD
-        )
-
-    def _retrieve_credentials_using(self, credential_process):
-        # We're not using shell=True, so we need to pass the
-        # command and all arguments as a list.
-        process_list = compat_shell_split(credential_process)
-        p = self._popen(process_list,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
-        stdout, stderr = p.communicate()
-        if p.returncode != 0:
-            raise CredentialRetrievalError(
-                provider=self.METHOD, error_msg=stderr.decode('utf-8'))
-        parsed = botocore.compat.json.loads(stdout.decode('utf-8'))
-        version = parsed.get('Version', '<Version key not provided>')
-        if version != 1:
-            raise CredentialRetrievalError(
-                provider=self.METHOD,
-                error_msg=("Unsupported version '%s' for credential process "
-                           "provider, supported versions: 1" % version))
-        try:
-            return {
-                'access_key': parsed['AccessKeyId'],
-                'secret_key': parsed['SecretAccessKey'],
-                'token': parsed.get('SessionToken'),
-                'expiry_time': parsed.get('Expiration'),
-            }
-        except KeyError as e:
-            raise CredentialRetrievalError(
-                provider=self.METHOD,
-                error_msg="Missing required key in response: %s" % e
-            )
-
-    @property
-    def _credential_process(self):
-        if self._loaded_config is None:
-            self._loaded_config = self._load_config()
-        profile_config = self._loaded_config.get(
-            'profiles', {}).get(self._profile_name, {})
-        return profile_config.get('credential_process')
 
 
 class InstanceMetadataProvider(CredentialProvider):
